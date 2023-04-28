39c3b05d816c6975cc933c0631430f280615cbee
sandra <nyamweasandra@gmail.com>
2023-04-29 01:02:34 +0300

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import CategoryFilter from "./CategoryFilter";
 import NewTaskForm from "./NewTaskForm";
 import TaskList from "./TaskList";
-
 import { CATEGORIES, TASKS } from "../data";
-console.log("Here's the data you're working with");
-console.log({ CATEGORIES, TASKS });
 
 function App() {
+  const [tasks, setTasks] = useState(TASKS);
+  const [selectedCategory, setSelectedCategory] = useState("All");
+
+  const onTaskFormSubmit = (newTask) => {
+    setTasks([...tasks, newTask]);
+  };
+
+  const onDeleteTask = (taskToDelete) => {
+    const updatedTasks = tasks.filter((task) => task !== taskToDelete);
+    setTasks(updatedTasks);
+  };
+
+  const onCategorySelect = (category) => {
+    setSelectedCategory(category);
+  };
+
+  let tasksToDisplay = tasks;
+  if (selectedCategory !== "All") {
+    tasksToDisplay = tasks.filter(
+      (task) => task.category === selectedCategory
+    );
+  }
+
   return (
     <div className="App">
       <h2>My tasks</h2>
-      <CategoryFilter />
-      <NewTaskForm />
-      <TaskList />
+      <CategoryFilter
+        categories={CATEGORIES}
+        selectedCategory={selectedCategory}
+        onSelectCategory={onCategorySelect}
+      />
+      <NewTaskForm categories={CATEGORIES} onTaskFormSubmit={onTaskFormSubmit} />
+      <TaskList tasks={tasksToDisplay} onDeleteTask={onDeleteTask} />
     </div>
   );
 }
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+import Task from "./Task";
 
-function TaskList() {
+function TaskList({ tasks }) {
+  const [selectedCategory, setSelectedCategory] = useState("All");
+  const [updatedTasks, setTasks] = useState(tasks);
+
+  const handleCategorySelect = (category) => {
+    setSelectedCategory(category);
+  };
+
+  const handleDeleteTask = (taskToDelete) => {
+    const newTasks = tasks.filter((task) => task !== taskToDelete);
+    setTasks(newTasks);
+  };
+
+  let displayedTasks = updatedTasks;
+
+  if (selectedCategory !== "All") {
+    displayedTasks = updatedTasks.filter((task) => task.category === selectedCategory);
+  }
+
   return (
     <div className="tasks">
-      {/* display a list of tasks using Task component */}
+      {displayedTasks.map((task) => (
+        <Task key={task.text} task={task} onDeleteTask={handleDeleteTask} />
+      ))}
     </div>
   );
 }
